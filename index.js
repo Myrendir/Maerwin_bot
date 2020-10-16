eec34d52b48f0c7dc877f4ffb43ba8d8f8e86d9d
Armand_D <armand.deshais@mail.com>
2020-10-16 11:33:16 +0200

    init node

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+const fs = require('fs')
+client.login(TOKEN).then();
+client.on('ready', () => {
+    console.info(`Logged in as ${client.user.tag}`);
+    client.user.setActivity('', {type: 'PLAYING'}).then();
+})
